e759add4fd7c9afdae3298799373849775dce376
SAPMANPC\Mosacho <sapirush97@gmail.com>
2021-03-04 19:21:18 +0200

    Added /env

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-  res.send(process.env.MESSAGE || 'HELLO WORLD');
-})
+app.get("/env", (req, res) => {
+  res.send(process.env);
+});
+app.get("/", (req, res) => {
+  res.send(process.env.MESSAGE || "HELLO WORLD");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at port ${port}`)
-})
+  console.log(`Example app listening at port ${port}`);
+});
